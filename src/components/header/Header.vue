58305de6a2d70ae22103bf21ea8345b331f1cd46
Datnq <dat@beedu.vn>
2021-09-14 14:55:14 +0700

    header

---
+++ src/components/header/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="z-header">
+    <el-input placeholder="Nhập tên bài hát, nghệ sĩ hoặc MV…" class="z-header__search base-input-nonebg" prefix-icon="el-icon-search" v-model="search"> </el-input>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator'
+
+  @Component
+  export default class ZHeader extends Vue {
+    search = ''
+  }
+</script>
+
+<style scoped lang="scss">
+  .z-header {
+    &__search {
+      width: 346px;
+      background: var(--alpha-bg);
+      border-radius: 30px;
+    }
+  }
+</style>
