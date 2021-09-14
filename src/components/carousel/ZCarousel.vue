496f77b022b1735baccc0c344f213c3d1e0a9d9e
Datnq <dat@beedu.vn>
2021-09-14 14:54:08 +0700

    comp carousel

---
+++ src/components/carousel/ZCarousel.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-carousel :interval="4000" type="card">
+    <el-carousel-item v-for="(item, index) in banner.items" :key="index">
+      <img :src="item.banner" :alt="item.alias" class="img-fluid" />
+    </el-carousel-item>
+  </el-carousel>
+</template>
+
+<script lang="ts">
+  import { Component, Prop, Vue } from 'vue-property-decorator'
+
+  @Component
+  export default class ZCarousel extends Vue {
+    @Prop({ required: true, type: Object, default: {} }) banner!: Record<string, any>
+  }
+</script>
+
+<style scoped></style>
